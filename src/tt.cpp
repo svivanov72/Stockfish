123156d81ac7f66b1c2497979928a34da60da5b6
Sergei Ivanov <srg.ivanov@gmail.com>
2019-06-06 00:30:04 +0300

    Take 2: simplified logic
    Bench: 3447564

--- src/tt.cpp
+++ src/tt.cpp
@@ -123,11 +123,13 @@ TTEntry* TranspositionTable::probe(const Key key, bool& found) const {
   const uint16_t key16 = key >> 48;  // Use the high 16 bits as key inside the cluster
 
   for (int i = 0; i < ClusterSize; ++i)
-      if (!tte[i].depth8 || tte[i].key16 == key16)
+      if (!tte[i].depth8)
+          return found = false, &tte[i];
+      else if (tte[i].key16 == key16)
       {
           tte[i].genBound8 = uint8_t(generation8 | (tte[i].genBound8 & 0x7)); // Refresh
 
-          return found = (bool)tte[i].depth8, &tte[i];
+          return found = true, &tte[i];
       }
 
   // Find an entry to be replaced according to the replacement strategy
